62ffab5865fbb6e54c58d82aa4eff6d6c520671a
Md. Ismail <ismailcse14@gmail.com>
2019-07-23 16:58:11 +0600

    Student record can store, update ,delete in simple project

---
+++ Student Record/Student Record/StudentRecord.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace Student_Record
+{
+    partial class StudentRecord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.updateIdTextbox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.updateDepartmentTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.departmentTextBox = new System.Windows.Forms.TextBox();
+            this.updateNameTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.ShowButton = new System.Windows.Forms.Button();
+            this.viewTableButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.nameTextBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.departmentTextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.idTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.saveButton);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(308, 299);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Information Table";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Department";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ID";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.Location = new System.Drawing.Point(204, 200);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(102, 32);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UpdateButton.Location = new System.Drawing.Point(222, 200);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(84, 32);
+            this.UpdateButton.TabIndex = 0;
+            this.UpdateButton.Text = "Update";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 20);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "ID";
+            // 
+            // updateIdTextbox
+            // 
+            this.updateIdTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateIdTextbox.Location = new System.Drawing.Point(132, 64);
+            this.updateIdTextbox.Name = "updateIdTextbox";
+            this.updateIdTextbox.Size = new System.Drawing.Size(174, 26);
+            this.updateIdTextbox.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Department";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idTextBox.Location = new System.Drawing.Point(132, 64);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(174, 26);
+            this.idTextBox.TabIndex = 3;
+            // 
+            // updateDepartmentTextBox
+            // 
+            this.updateDepartmentTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateDepartmentTextBox.Location = new System.Drawing.Point(132, 158);
+            this.updateDepartmentTextBox.Name = "updateDepartmentTextBox";
+            this.updateDepartmentTextBox.Size = new System.Drawing.Size(174, 26);
+            this.updateDepartmentTextBox.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Name";
+            // 
+            // departmentTextBox
+            // 
+            this.departmentTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.departmentTextBox.Location = new System.Drawing.Point(132, 158);
+            this.departmentTextBox.Name = "departmentTextBox";
+            this.departmentTextBox.Size = new System.Drawing.Size(174, 26);
+            this.departmentTextBox.TabIndex = 5;
+            // 
+            // updateNameTextBox
+            // 
+            this.updateNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateNameTextBox.Location = new System.Drawing.Point(132, 109);
+            this.updateNameTextBox.Name = "updateNameTextBox";
+            this.updateNameTextBox.Size = new System.Drawing.Size(174, 26);
+            this.updateNameTextBox.TabIndex = 7;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameTextBox.Location = new System.Drawing.Point(132, 109);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(174, 26);
+            this.nameTextBox.TabIndex = 7;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ShowButton);
+            this.groupBox2.Controls.Add(this.DeleteButton);
+            this.groupBox2.Controls.Add(this.updateNameTextBox);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.updateDepartmentTextBox);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.updateIdTextbox);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.UpdateButton);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(367, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(318, 299);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Information Table";
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteButton.Location = new System.Drawing.Point(121, 200);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(86, 32);
+            this.DeleteButton.TabIndex = 8;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // ShowButton
+            // 
+            this.ShowButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ShowButton.Location = new System.Drawing.Point(10, 200);
+            this.ShowButton.Name = "ShowButton";
+            this.ShowButton.Size = new System.Drawing.Size(90, 32);
+            this.ShowButton.TabIndex = 9;
+            this.ShowButton.Text = "Show";
+            this.ShowButton.UseVisualStyleBackColor = true;
+            this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
+            // 
+            // viewTableButton
+            // 
+            this.viewTableButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewTableButton.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.viewTableButton.Location = new System.Drawing.Point(12, 317);
+            this.viewTableButton.Name = "viewTableButton";
+            this.viewTableButton.Size = new System.Drawing.Size(247, 47);
+            this.viewTableButton.TabIndex = 2;
+            this.viewTableButton.Text = "View Table";
+            this.viewTableButton.UseVisualStyleBackColor = true;
+            this.viewTableButton.Click += new System.EventHandler(this.ViewTableButton_Click);
+            // 
+            // StudentRecord
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 389);
+            this.Controls.Add(this.viewTableButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "StudentRecord";
+            this.Text = "Student Record";
+            this.Load += new System.EventHandler(this.StudentRecord_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox departmentTextBox;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox updateIdTextbox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox updateDepartmentTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox updateNameTextBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button ShowButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button viewTableButton;
+    }
+}
+
---
+++ Student Record/Student Record/StudentRecord.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student_Record
+{
+    public partial class StudentRecord : Form
+    {
+        public StudentRecord()
+        {
+            InitializeComponent();
+        }
+
+        private void StudentRecord_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(idTextBox.Text))
+            {
+                MessageBox.Show("Id cannot empty!");
+                return;
+            }
+            if (String.IsNullOrEmpty(nameTextBox.Text))
+            {
+                MessageBox.Show("Name cannot empty!");
+                return;
+            }
+            if (String.IsNullOrEmpty(departmentTextBox.Text))
+            {
+                MessageBox.Show("Department cannot  empty!");
+                return;
+            }
+            try
+            {
+                int count = 0;
+                //1. connentionString
+                string connentionString = @"Server = DESKTOP-3K97P4H\SQLEXPRESS; Database =StudentDataBase; Integrated security = true";
+                SqlConnection sqlConnection = new SqlConnection();
+                sqlConnection.ConnectionString = connentionString;
+
+                //2 connection open
+                sqlConnection.Open();
+
+                //3. command string
+                string commandString = "Insert into Students Values(" + idTextBox.Text + ",'" + nameTextBox.Text + "','" + departmentTextBox.Text + "')";
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.CommandText = commandString;
+                sqlCommand.Connection = sqlConnection;
+
+                //4. Execute
+                count = sqlCommand.ExecuteNonQuery();
+
+                if (count > 0)
+                {
+                    MessageBox.Show("Data insert successful");
+                }
+                else
+                {
+                    MessageBox.Show("Data insert Failed");
+                }
+                //5. connection close
+                sqlConnection.Close();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(updateIdTextbox.Text))
+            {
+                MessageBox.Show("Id cannot empty!");
+                return;
+            }
+            
+            try
+            {
+                string connectionstring = @"Server =DESKTOP-3K97P4H\SQLEXPRESS ; Database =StudentDataBase ; Integrated Security = true";
+                SqlConnection sqlConnection = new SqlConnection();
+                sqlConnection.ConnectionString = connectionstring;
+
+                sqlConnection.Open();
+
+                string commandString = "Delete from Students where id = " + updateIdTextbox.Text + "";
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.CommandText = commandString;
+                sqlCommand.Connection = sqlConnection;
+                int count = 0;
+               count = sqlCommand.ExecuteNonQuery();
+                if (count > 0)
+                {
+                    MessageBox.Show("Delete successful");
+                }
+                else
+                {
+                    MessageBox.Show("Delete Failed");
+                }
+
+
+                sqlConnection.Close();
+            }
+
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+
+
+            if (String.IsNullOrEmpty(updateIdTextbox.Text))
+            {
+                MessageBox.Show("Id cannot empty!");
+                return;
+            }
+            if (String.IsNullOrEmpty(updateNameTextBox.Text))
+            {
+                MessageBox.Show("Name cannot empty!");
+                return;
+            }
+            if (String.IsNullOrEmpty(updateDepartmentTextBox.Text))
+            {
+                MessageBox.Show("Department cannot  empty!");
+                return;
+            }
+
+            try
+            {
+                //connetion string
+                string connectionString = @"Server = DESKTOP-3K97P4H\SQLEXPRESS; Database = StudentDataBase; Integrated Security = true";
+                SqlConnection sqlConnection = new SqlConnection();
+                sqlConnection.ConnectionString = connectionString;
+
+                //connection open
+                sqlConnection.Open();
+
+                //command string
+
+                string commandString = "Update Students set id =" + updateIdTextbox.Text + ", Name ='" + updateNameTextBox.Text + "', Department = '" + updateDepartmentTextBox.Text + "' where id = "+ updateIdTextbox.Text+ "";
+
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.CommandText = commandString;
+                sqlCommand.Connection = sqlConnection;
+
+                // execute
+
+                int count = 0;
+                count = sqlCommand.ExecuteNonQuery();
+
+                if (count > 0)
+                {
+                    MessageBox.Show("Data updated successful");
+                }
+                else
+                {
+                    MessageBox.Show("Data updated Failed");
+                }
+
+
+                // connection close
+
+                sqlConnection.Close();
+            }
+            catch (Exception exeption)
+            {
+                MessageBox.Show(exeption.Message);
+            }
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+
+            if (String.IsNullOrEmpty(updateIdTextbox.Text))
+            {
+                MessageBox.Show("Id cannot empty!");
+                return;
+            }
+
+            try
+            {
+                //connetion string
+                string connectionString = @"Server = DESKTOP-3K97P4H\SQLEXPRESS; Database = StudentDataBase; Integrated Security = true";
+                SqlConnection sqlConnection = new SqlConnection();
+                sqlConnection.ConnectionString = connectionString;
+
+                //connection open
+                sqlConnection.Open();
+
+                //command string
+
+                string commandString = "Select * from Students where id = "+ updateIdTextbox.Text+"";
+
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.CommandText = commandString;
+                sqlCommand.Connection = sqlConnection;
+
+                // execute
+
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+
+
+                if (dataTable.Rows.Count>0)
+                {
+                    updateNameTextBox.Text = dataTable.Rows[0]["Name"].ToString();
+                    updateDepartmentTextBox.Text = dataTable.Rows[0]["Department"].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Data show Failed");
+                }
+
+
+                // connection close
+
+                sqlConnection.Close();
+            }
+            catch (Exception exeption)
+            {
+                MessageBox.Show(exeption.Message);
+            }
+        }
+
+        private void ViewTableButton_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                //connetion string
+                string connectionString = @"Server = DESKTOP-3K97P4H\SQLEXPRESS; Database = StudentDataBase; Integrated Security = true";
+                SqlConnection sqlConnection = new SqlConnection();
+                sqlConnection.ConnectionString = connectionString;
+
+                //connection open
+                sqlConnection.Open();
+
+                //command string
+
+                string commandString = "Select * from Students";
+
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.CommandText = commandString;
+                sqlCommand.Connection = sqlConnection;
+
+                // execute
+
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+
+                int i = dataTable.Rows.Count;
+                string allInfor = "Name\t\tDepartment\n";
+                if (dataTable.Rows.Count > 0)
+                {
+                    while (i>0)
+                    {
+                        allInfor += dataTable.Rows[i-1]["Name"].ToString()+"\t\t" + dataTable.Rows[i-1]["Department"].ToString()+" \n";
+                        i--;
+                    }
+
+                    MessageBox.Show(allInfor);
+                }
+                else
+                {
+                    MessageBox.Show("Data show Failed");
+                }
+
+
+                // connection close
+
+                sqlConnection.Close();
+            }
+            catch (Exception exeption)
+            {
+                MessageBox.Show(exeption.Message);
+            }
+
+        }
+    }
+}
